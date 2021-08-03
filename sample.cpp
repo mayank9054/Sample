e92145ed3fb13b4e3773e7dab419ae6145b38aa8
Mayank Chhatraya <73630309+mayank9054@users.noreply.github.com>
2021-08-03 17:13:32 +0530

    Revert "subtraction added "

--- sample.cpp
+++ sample.cpp
@@ -7,18 +7,13 @@ int main(){
     cout << "Enter two number";
     cin >> a;
     cin >> b;
-    cout<<" 1.Addition \n 2.Subtraction \n 3.multiplication \n 4.division \n";
     cout << "Enter the value";
-
     cin >> value;
 
     switch (value){
        case 1 :
            cout << "additon :" << a + b;
            break;
-       case 2:
-            cout<<"Minus : "<< a - b;
-            break;
 
         default:
             cout << "Invalid Choice";
